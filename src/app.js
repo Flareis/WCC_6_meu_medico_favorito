ca3aa2c9736666de8ee6851d49afd4ff0c1a16f5
FlaviaDosReis <reis.dos.flavia@gmail.com>
2021-11-25 01:35:00 -0300

    My favotite doctor - part 2

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(function(req, res, next){
+    res.header ("Access-Control-Allow-Origin", "*");
+    res.header ("Access-Control-Allow-Hearders", "Origin, X-Requested-With, Content-Type, Accept");
+    
+    next();
+});
+
+app.options("/*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers")
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+    );
+    res.send("send some thing whatever")
+})
+
+module.exports = {
+    app
+}
